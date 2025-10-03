df834c244d33a4d8a131821518544f27fbf3c8ab
Алексей Елютин <elyutin91@mail.ru>
2025-10-03 12:59:38 +0300

    Updating second of the source Lambda_express_for_interface

--- Lambda_express_for_interface/src/Main.java
+++ Lambda_express_for_interface/src/Main.java
@@ -15,8 +15,14 @@ public class Main {
         Predicate<String> predicate1 = Objects::nonNull;
         System.out.println(predicate1.test(s));
         Predicate<String> predicate2 = t -> t.contains("H");
-
         System.out.println(predicate1.and(predicate2).test(s));
+
+        s = "Java data";
+        printable.print(s);
+        Predicate<String> predicate3 = t -> t.contains("J");
+        Predicate<String> predicate4 = t -> t.contains("N");
+        Predicate<String> predicate5 = t -> t.contains("a");
+        System.out.println(predicate3.or(predicate4).and(predicate5).test(s));
         /* for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
