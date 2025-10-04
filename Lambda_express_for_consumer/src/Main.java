8ae4942d19d16bd85eb913b78773170f3585ebe1
Алексей Елютин <elyutin91@mail.ru>
2025-10-04 12:06:29 +0300

    Updating of the source Lambda_express_for_consumer

--- Lambda_express_for_consumer/src/Main.java
+++ Lambda_express_for_consumer/src/Main.java
@@ -1,4 +1,6 @@
-import java.util.function.Consumer;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.function.Consumer;
+import java.util.function.Function;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
@@ -6,13 +8,21 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         //System.out.printf("Hello and welcome!");
         HeavyBox heavyBox = new HeavyBox();
-        // HeavyBox heavyBox2 = new HeavyBox();
         Consumer<String> consumer1 = System.out::println;
         consumer1.accept(heavyBox.message1);
         Consumer<String> consumer2 = str -> System.out.println(str.toUpperCase());
         consumer2.accept(heavyBox.message2);
         consumer1.andThen(consumer2).accept(heavyBox.message1+" "+heavyBox.message2);
 
+        Function<Integer,String> function = numb -> {
+            if  (numb<0) return "Отрицательное число";
+            else if (numb>0) return "Положительное число";
+            else if (numb==0) return "Ноль";
+            return "";
+        };
+        System.out.println('\n'+function.apply(10));
+        System.out.println(function.apply(-5));
+        System.out.println(function.apply(0));
         /*  “Отгрузили ящик с весом n”
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
